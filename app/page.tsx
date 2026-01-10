032022e38d3a085c64ad14d559658cba6a564a4d
Leonardo Sanner <leosanner12@gmail.com>
2026-01-10 19:24:35 -0300

    ux components and config user role

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
 import { signOutAction } from "./actions/auth";
+import { getSession } from "@/lib/auth-session";
 
 export default async function Home() {
-	const session = await auth.api.getSession({
-		headers: await headers(),
-	});
-
-	console.log(session);
+	const session = await getSession();
 
 	return (
 		<div>
